e16c8bc1771479be906506ba2d4d1ad6f26e1f48
Gupta <nidhi.gupta@coxautoinc.com>
2022-02-24 11:36:16 -0500

    initial commit

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "region" { default = "us-east-1" }
+variable "environment" { default = "" }
+variable "project" { default = "javaweb" }
+variable "platform" { default = "Devops" }
+variable "vpc_id" { default = "vpc-072ecaa60abe662d4" }
+variable "team" { default = "devops4solutions" }
