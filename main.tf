390ed41808323df419de01a8d41318a0326ef3c4
shray9 <107247048+shray9@users.noreply.github.com>
2022-07-22 19:56:44 +0530

    dfsa
    gda

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
   name = "${var.project}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b"]
+  azs             = ["us-east-1c", "us-east-1e"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
 }
